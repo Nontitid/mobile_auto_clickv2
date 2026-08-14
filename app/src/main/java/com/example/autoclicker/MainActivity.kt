665c0d9a2b3f13d887efbc448aba3cd0e89b6e76
Nontitid Vangtal <nontitidvangtal@gmail.com>
2026-08-14 18:03:02 +0700

    Add app/src/main/java/com/example/autoclicker/MainActivity.kt

---
+++ app/src/main/java/com/example/autoclicker/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.autoclicker
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnOpenAccessibility).setOnClickListener {
+            startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+        }
+    }
+}
